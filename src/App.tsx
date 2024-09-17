4ba79dea95f6d57122055803e2e5d263bfdbf341
FlorSantamaria <florenciasantamaria98@gmail.com>
2024-09-17 12:44:18 -0300

    modo claro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import './App.css';
 import { HomePage } from './page/Home';
 
 function App() {
+
+  useEffect(() => {
+    document.documentElement.classList.remove('dark');
+    document.documentElement.classList.add('light');
+  }, []);
   return (
     <div className="App">
       <HomePage />
