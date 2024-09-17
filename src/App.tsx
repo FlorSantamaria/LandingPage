4872de34dc930bf167aa4819637c9b1fa6e2fe14
FlorSantamaria <florenciasantamaria98@gmail.com>
2024-09-17 12:51:14 -0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { HomePage } from './page/Home';
 
 function App() {
 
-  useEffect(() => {
-    document.documentElement.classList.remove('dark');
-    document.documentElement.classList.add('light');
-  }, []);
   return (
     <div className="App">
       <HomePage />
